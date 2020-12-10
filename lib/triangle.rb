80a07f8220f8fe220a3b4f68cb3863f1d870d3a4
Aiden <aiden.marques94@gmail.com>
2020-12-09 19:27:51 -0500

    commit tree dbc2a4ac052eb06d488b242fd42798bf8dc2261a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end 
+
+  def kind 
+    validate
+    if ((a == b) && (a == c) && (c == b)) 
+      :equilateral
+    elsif ((a == b) || (b == c) || (c == a)) 
+      :isosceles
+    else
+      :scalene
+    end 
+  end
+
+  def validate
+    triangle = [(a+b>c),(a + c > b), (b + c > a)]
+    [a,b,c].each do |side| 
+      triangle << false if side <= 0
+    raise TriangleError if triangle.include?(false)
+  end 
+end 
+  class TriangleError < StandardError
+
+  end 
 end
